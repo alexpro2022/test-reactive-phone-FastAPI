157c2c4dae70f1dc86cab44a1e1f72894cf9b0b4
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-12-08 20:57:10 +0300

    Added password validators tests

--- app/validators.py
+++ app/validators.py
@@ -5,12 +5,9 @@ from app.core import settings
 
 def password_length_validator(password: str):
     if len(password) < settings.password_length:
-        raise InvalidPasswordException(
-            f'Пароль должен быть длиной не менее '
-            f'{settings.password_length} символов.')
+        raise InvalidPasswordException(f'Пароль должен быть длиной не менее {settings.password_length} символов.')
 
 
 def password_content_validator(password: str, email: str):
     if email in password:
-        raise InvalidPasswordException(
-            'В пароле не должно содержаться e-mail.')
+        raise InvalidPasswordException('В пароле не должно содержаться e-mail.')
