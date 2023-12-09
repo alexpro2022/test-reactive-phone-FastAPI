2a11ecf7f528055b62c199f08dd8a07ac79f0080
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-12-09 23:29:17 +0300

    Added celery task testing

--- tests/unit_tests/test_task.py
+++ tests/unit_tests/test_task.py
@@ -1,5 +1,8 @@
+from celery.local import PromiseProxy
+
 from app.celery_tasks.tasks import synchronize
 
 
-def test_task_name():
+def test_task():
+    assert isinstance(synchronize, PromiseProxy), type(synchronize)
     assert synchronize.name == 'app.celery_tasks.tasks.synchronize'
